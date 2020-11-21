3cfa010631cf533e030a408ce5e152deb3c063bc
PrinceMargaret <princeagrahari2000@gmail.com>
2020-11-21 11:53:29 +0530

    varify user given otp

---
+++ otp_varify.py
@@ -0,0 +1,38 @@
+#!C:\Users\PrinceMargaret\anaconda3\python.exe
+print("content-type:text/html\n\n")
+__author__="Prince"
+
+import cgi,mysql.connector as mysql
+
+#===========================================================================================================
+
+form=cgi.FieldStorage()
+
+email=form.getvalue("email")
+                                                                                               #variables
+
+OTP=form.getvalue("OTP")
+
+
+email="princeagrahari2000@gmail.com"
+OTP='margaret'
+#============================================================================================================
+
+#===========================================================================================================================
+db = mysql.connect(
+    host = "localhost",
+    user = "root",
+    database="mysql"
+    
+)
+                                                                                                     #database
+cursor = db.cursor()
+
+
+cursor.execute("update user_data set user_otp=%s where email=%s",(OTP,email))
+db.commit()
+db.close()
+cursor.close()
+
+#=================================================================================================================
+  
